85a5fad33344285ea7cb692bde9e687b3e26a79e
Sohel Rana <sohel.rana@ergo-ventures.com>
2019-05-27 13:37:56 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const GoogleSpreadsheet = require('google-spreadsheet');
+const {promisify} = require('util');
+
+const creds = require('./client_secret');
+
+function printEmployee(employee){
+    console.log(`Name: ${employee.name}`);
+    console.log(`Designation: ${employee.designation}`);
+    console.log(`Department: ${employee.department}`);
+    console.log('-----------------------')
+}
+
+async function accessSpreadsheet(){
+    const doc = new GoogleSpreadsheet('1YGyS1W8fQumpUInv8bDvXdQUszPMRcvNNbwV6dqg790');
+    await promisify(doc.useServiceAccountAuth)(creds);
+    const info = await promisify(doc.getInfo)();
+    const sheet = info.worksheets[0];
+    const rows = await promisify(sheet.getRows)({
+        offset:1
+    });
+    //console.log(rows)
+    const newEmployee ={
+        name: 'Md Musfiqul Islam',
+        designation: "SW",
+        department: "EVL"
+    };
+
+    await promisify(sheet.addRow)(newEmployee);
+
+    rows.forEach(row => {
+        printEmployee(row);
+    });
+} 
+
+
+
+accessSpreadsheet();
